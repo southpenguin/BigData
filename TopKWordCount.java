7c894e1481dfabe681adb3db34986ac21af0e7b1
Xin He <southpenguin@Xins-MacBook-Pro-2.local>
2014-10-08 23:55:13 -0400

    FixedLengthWordCount

---
+++ TopKWordCount.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        
+public class TopKWordCount {
+	
+	public static final int K = 100;
+	
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				if (word.toString().length() == 7) {
+					context.write(word, one);
+				}
+			}
+		} 
+	} 
+
+	
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private TreeMap<Integer, String> topK = new TreeMap<Integer, String>();
+		
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			topK.put(sum, key.toString());
+			if (topK.size() > K){
+				topK.remove(topK.firstKey());
+			}
+		}
+		
+		@Override
+		protected void cleanup(Context context) throws IOException, InterruptedException{
+			for (int s : topK.descendingMap().keySet()){
+				context.write(new Text(topK.get(s)), new IntWritable(s));
+			}
+		}
+	}
+
+
+    		
+        
+ public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+        
+        Job job = new Job(conf, "Top-K Word Count");
+    
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+        
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+        
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    job.setNumReduceTasks(1);
+    job.setJarByClass(TopKWordCount.class);
+        
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+    job.waitForCompletion(true);
+ }
+        
+}
